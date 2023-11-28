dde49aedeac5419f86026329717d90f11aa99eae
Rob Castellano <31711049+castellanorob@users.noreply.github.com>
2023-11-27 21:46:55 -0600

    Updated index.js

--- campaignJournal/server/index.js
+++ campaignJournal/server/index.js
@@ -30,7 +30,7 @@ app.use("/Blocked", blockedRouter);
 app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build', 'app.js'));
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 db.sequelize.sync().then(() => {
