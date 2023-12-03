ce9906f3ccaa979e8d7caac15f7a5d8a1af098fe
dnzegeye <89752582+dnzegeye@users.noreply.github.com>
2023-12-03 11:47:23 -0600

    Update APIURL.js
    Updated apirul to work with app

--- campaignJournal/client/src/helpers/APIURL.js
+++ campaignJournal/client/src/helpers/APIURL.js
@@ -1,1 +1,1 @@
-export const APIURL = `${process.env.REACT_APP_ReactAppURL}/` || 'http://localhost:3001/';
+export const APIURL = process.env.REACT_APP_ReactAppURL ? `${process.env.REACT_APP_ReactAppURL}/` : 'http://localhost:3001/';
